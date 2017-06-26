24a55efd31c56e3978fb7ccec588463a4fddb713
Jason Ainsworth and Kat Gifford <jason+kat@>
2017-06-26 16:05:54 -0700

    add edit/delete functs to sightings

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -19,6 +19,29 @@
      end
    end
 
+   def edit
+     @animal = Animal.find(params[:animal_id])
+     @sighting = Sighting.find(params[:id])
+     render :edit
+   end
+
+   def update
+     @animal = Animal.find(params[:animal_id])
+     @sighting = Sighting.find(params[:id])
+     if @sighting.update(sighting_params)
+       redirect_to animal_path(@sighting.animal)
+     else
+       render :edit
+     end
+   end
+
+   def destroy
+     @animal = Animal.find(params[:animal_id])
+     @sighting = Sighting.find(params[:id])
+     @sighting.destroy
+     redirect_to animal_path
+   end
+
 private
   def sighting_params
     params.require(:sighting).permit(:date, :lat, :lng, :region)
